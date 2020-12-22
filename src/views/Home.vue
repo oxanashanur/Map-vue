f1966c84016829ff45c5bed9df3ced742cb0c874
Oxana Bokievets <oxanaboki@gmail.com>
2020-12-22 23:25:58 +1000

    New app

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <router-link to="/map" class="router-link">Map</router-link>
+      <hr>
+      <TaskList />
+  </div>
+</template>
+
+<script>
+import TaskList from '@/components/TaskList'
+export default {
+  name: 'App',
+  components: {
+    TaskList
+  }
+}
+</script>
+
+<style>
+.router-link {
+  text-decoration: none;
+  color: #2c3e50;
+}
+.router-link:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/views/Map.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <router-link to="/" class="router-link">Task</router-link>
+    <hr>
+    <MapList />
+  </div>
+</template>
+
+<script>
+import MapList from '@/components/MapList'
+export default {
+  name: 'App',
+  components: {
+    MapList
+  }
+}
+</script>
+
+<style>
+.router-link {
+  text-decoration: none;
+  color: #2c3e50;
+}
+.router-link:hover {
+  text-decoration: underline;
+}
+</style>
